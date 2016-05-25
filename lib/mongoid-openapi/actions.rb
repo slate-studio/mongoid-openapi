6d47d7e4786abf45808fb86f178a6d66ac860cc8
Alexander Kravets <a@kra.vc>
2016-05-25 16:23:27 +0300

    Update local variable name in actions

--- lib/mongoid-openapi/actions.rb
+++ lib/mongoid-openapi/actions.rb
@@ -18,44 +18,44 @@ module MongoidOpenApi
       end
 
       def show
-        object = find_object
-        object = get_object_version(object)
-        render json: object.as_json(json_config(:show))
+        @object = find_object
+        @object = get_object_version(@object)
+        render json: @object.as_json(json_config(:show))
       end
 
       def create
-        object = build_object
-        if object.save
-          render json: object.as_json(json_config(:create))
+        @object = build_object
+        if @object.save
+          render json: @object.as_json(json_config(:create))
         else
           if Rails.env.development?
-            logger.info "Errors:\n  #{object.errors.to_h}"
+            logger.info "Errors:\n  #{@object.errors.to_h}"
           end
-          render json: object.errors, status: :unprocessable_entity
+          render json: @object.errors, status: :unprocessable_entity
         end
       end
 
       def update
-        object = find_object
-        if object.update_attributes(resource_params)
-          render json: object.as_json(json_config(:update))
+        @object = find_object
+        if @object.update_attributes(resource_params)
+          render json: @object.as_json(json_config(:update))
         else
           if Rails.env.development?
-            logger.info "Errors:\n  #{object.errors.to_h}"
+            logger.info "Errors:\n  #{@object.errors.to_h}"
           end
-          render json: object.errors, status: :unprocessable_entity
+          render json: @object.errors, status: :unprocessable_entity
         end
       end
 
       def destroy
-        object = find_object
-        if object.destroy
+        @object = find_object
+        if @object.destroy
           render nothing: true, status: 204
         else
           if Rails.env.development?
-            logger.info "Errors:\n  #{object.errors.to_h}"
+            logger.info "Errors:\n  #{@object.errors.to_h}"
           end
-          render json: object.errors, status: :unprocessable_entity
+          render json: @object.errors, status: :unprocessable_entity
         end
       end
     end
